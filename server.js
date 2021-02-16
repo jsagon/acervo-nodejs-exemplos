5e15b5fe3c26f2243f9366d350a821a6d0f2da71
Jhonatan dos S. Gonçalves <jhonatanrj180@hotmail.com>
2021-02-16 18:28:48 -0300

    Servindo páginas HTML, layouts, handlebars, entre outros

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 // Requisita o módulo Express instalado
 const express = require('express')
+const exphbs  = require('express-handlebars');
 
 // Cria uma instância da aplicação/express
 const app = express()
+app.use('/public', express.static(__dirname + '/public'))
+
+app.engine('hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', 'hbs');
 
 // Registra uma rota get(sinalizando leitura) apontando para a raiz '/'. Ou seja, exemplo.com/
 // req: Request - Em resumo, corresponde as entradas, tudo aquilo que é enviado para o servidor 
 // res: Response - Corresponde as saídas, tudo aquilo que é deseja enviar para a exterior
 app.get('/', (req, res) => {
-    res.send('Olá mundo. <a href="/sobre">Sobre</a>')
+    res.render('index')
 })
 
 app.get('/sobre', (req, res) => {
-    res.send("<h1>Um simples tutorial de NodeJS.</h1>")
+    res.render('sobre')
 })
 
 // Inicializa o servidor observando a porta 3000
